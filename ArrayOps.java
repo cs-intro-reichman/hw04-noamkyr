6b4f9bd42df340ef5f5981268271322f313513f6
noam <noam.kyram@post.runi.ac.il>
2024-01-09 23:45:06 +0200

    documentation

--- ArrayOps.java
+++ ArrayOps.java
@@ -12,102 +12,165 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
+
+        // init variable holds if the missing value was found
         boolean has_missing_found = false;
+
+        // the value of the missing value
         int missing_val = -1;
+
+        // for every possible value 0 <= i <= array.length
         for (int i = 0; i <= array.length && !has_missing_found; i++) {
+
+            // init a variable that holds the index of the current value in the array
             int j = 0;
+
+            // go to the next index if the value wasnt found and it didnt finish search
             while(j < array.length && array[j] != i) {
                 j++;
             }
 
+            // if finished searching and the value wasnt found
             if (j == array.length) {
+
+                // update that the value was found
                 has_missing_found = true;
+
+                // update the value of the missing value
                 missing_val = i;
             }
 
         }
+
+        // return the missing value
         return missing_val;
     }
 
     public static int secondMaxValue(int [] array) {
+
+        // init the first max from the first 2 elements
         int max = Math.max(array[0],array[1]);
+
+        // init the first max from the first 2 elements
         int second_max = Math.min(array[0], array[1]);
 
+        // from the second element to the end of the array
         for (int i = 1; i < array.length; i++) {
+
+            // check if there is new max
             if (array[i] >= max){
+
+                // update the new max and the second max value
                 second_max = max;
                 max = array[i];
             }
         }
 
+        // return the second max
         return second_max;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
 
+
+        // ceck if all the elements in array 1 are in array 2
         for (int i = 0; i < array1.length; i++) {
+
+            // set value that the current element wasnt found in the second array
             boolean has_found = false;
 
+            // check if the current element of array 1 is in array 2
             for (int j = 0; j < array2.length && !has_found; j++) {
+
+                // check the values of the elements
                 if (array1[i] == array2[j]){
+
+                    // update that the current value was found
                     has_found = true;
                 }
             }
 
+            // return false if one of the elements isnt at the second array
             if (has_found == false){
                 return has_found;
             }
 
+            // reset the value for the next element
             has_found = false;
 
         }
 
+
+        // check if every value of the second array is at the first one
         for (int i = 0; i < array2.length; i++) {
+
+            // set value that the current element wasnt found in the second array
             boolean has_found = false;
 
+            // check if the current element of array 2 is in array 1
             for (int j = 0; j < array1.length && !has_found; j++) {
+
+                // check the values of the elements
                 if (array2[i] == array1[j]){
+
+                    // update that the current value was found
                     has_found = true;
                 }
             }
 
+            // return false if one of the elements isnt at the second array
             if (has_found == false){
                 return has_found;
             }
 
+            // reset the value for the next element
             has_found = false;
 
         }
+
+        // all the elements of array 2 are in array 1
         return true;
     }
 
     public static boolean isSorted(int [] array) {
 
+        // the array is sortes when it has 1 element
         if (array.length == 1){
             return true;
         }
 
+        // check if the array should be increasing or decreasing by the first 2 elements
+        // set the value if increasing or decreasing
         boolean is_increasing = true;
         if (array[1] < array[0]) {
             is_increasing = false;
         }
 
+        // check for increasing array
         for (int i = 1; i < array.length && is_increasing; i++) {
+
+            // when it isnt increasing
             if (array[i] < array[i-1]){
+                // not sorted
                 return false;
             }
         }
 
+        // the array is increasingly sorted
         if (is_increasing == true) {
             return true;
         }
 
+        // check for decreasing array
         for (int i = 1; i < array.length; i++) {
+
+            // return false if the array isn't decreasingly sorted
             if (array[i] > array[i-1]){
                 return false;
             }
         }
 
+        // the array is decreasingle sorted
         return true;
     }
 
--- StringOps.java
+++ StringOps.java
@@ -36,58 +36,92 @@ public class StringOps {
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
+        // init the string of the result
         String opposite = "";
-        String voules = "aeoui";
-        String capital_voules = "AOUEI";
+
+        // string with all the small voules
+        String vowels = "aeoui";
+
+        // string with all the capital vo
+        String capital_vowels = "AOUEI";
+
+        // check every letter in the string
         for (int i = 0; i < string.length(); i++) {
+
+            // the value of the current char
             char current = string.charAt(i);
+
+            // check if from the abc
             if ((current >= 'a' && current <= 'z') || (current >= 'A' && current <= 'Z')){
-                if (voules.indexOf(current) != -1){
-                   if (current >= 'a' && current <= 'z'){
+
+                // check if small vowel
+                if (vowels.indexOf(current) != -1){
+                    if (current >= 'a' && current <= 'z'){
+                        // make capital value
                         current -= 32;
                    }
                 } else {
-                    if (current >= 'A' && current <= 'Z' && capital_voules.indexOf(current) == -1){
+                    // make all the capital letters small if not vowels
+                    if (current >= 'A' && current <= 'Z' && capital_vowels.indexOf(current) == -1){
                         current += 32;
                     }
                 }
             }
+            // add the current letter to the new stirng
             opposite += current;
         }
 
+        // return the result
         return opposite;
     }
 
     public static String camelCase (String string) {
+
+        // set the value of the new string
         String new_string = "";
+
+        // count number of words, set 0
         int count_word = 0;
+
+        // for enery letter in the string
         for (int i = 0; i < string.length(); i++) {
+
+            // the value of the current letter
             char current = string.charAt(i);
+
+            // check for all the not space letters
             if (current != ' ') {
 
+                // check if is a start of a new word
                 if (i == 0 || string.charAt(i-1) == ' '){
                     count_word ++;
+
+                    // make small letter if the first letter in the first word
                     if (current >= 'A' && current <= 'Z' && count_word == 1){
                         current += 32;
                     }
 
+                    // update in case of first in a new word and it is not big
                     if (current >= 'a' && current <= 'z' && count_word > 1){
                         current -= 32;
                     }
 
                 } else {
 
+                    // make all the letter small in the first word
                     if (count_word == 1){
                         if (current >= 'A' && current <= 'Z'){
                             current += 32;
                         }
                     }
 
+
                     if (current >= 'A' && current <= 'Z'){
                         current += 32;
                     }
                 }
+
+                // add the current letter to the string
                 new_string += current;
 
             }
@@ -95,33 +129,46 @@ public class StringOps {
 
         }
 
-
+        // return the result
         return new_string;
     }
 
     public static int[] allIndexOf (String string, char chr) {
+
+        // set value of a counter
         int count = 0;
 
+        // for all the elements in the string check the number of time the chr is in the string
         for (int i = 0; i < string.length(); i++) {
+
+            // the value of the current char
             char current = string.charAt(i);
 
+            // add to the counter it is now has chr value
             if (current == chr){
                 count ++;
             }
 
         }
 
+        // set new array with size of the counter
         int indexes [] = new int[count];
+
+        // set counter of the current index of the new array
         int count_current_add = 0;
 
+        // for every letter in the string
         for (int i = 0; i < string.length(); i++) {
+
+            // add the current index of the letter if there is a match in the current avilable place
             if (string.charAt(i) == chr){
                 indexes[count_current_add] = i;
+                // set new avilable index
                 count_current_add++;
             }
         }
         
-        
+        // return the new array
         return indexes;
     }
 }
